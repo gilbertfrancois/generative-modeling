25e2051c273751313e0b4d2cf24299dd7a145b74
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2020-02-24 22:54:10 +0100

    Adds test for GMM sampler.

---
+++ tests/test_gmm_sampler.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from bayes_classifier.bayes_classifier_gmm import BayesClassifierGMM
+
+
+def main():
+
+    # Load data
+    data = pd.read_csv("../data/train.csv")
+    Y = data.iloc[:, 0].to_numpy()
+    X = data.iloc[:, 1:].to_numpy()
+    X = X / 255
+
+    # Create the generative model.
+    clf = BayesClassifierGMM()
+    clf.fit(X, Y)
+
+    fig, axs = plt.subplots(2, 10, figsize=(10, 2))
+    for k in range(10):
+        # sample from a given cluster k.
+        res = clf.sample_given_y(k)
+        axs[0, k].imshow(res["sample"].reshape(28, 28))
+        axs[1, k].imshow(res["mean"].reshape(28, 28))
+        axs[0, k].axis("off")
+        axs[1, k].axis("off")
+    fig.suptitle("Sampling $P(x|y)$ from a Gaussian Mixture Model")
+    plt.show()
+
+    # Sample from a random class.
+    res = clf.sample()
+    fig, axs = plt.subplots(2, 1, figsize=(4, 8))
+    axs[0].imshow(res["sample"].reshape(28, 28))
+    axs[1].imshow(res["mean"].reshape(28, 28))
+    axs[0].set_title(res["class"])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
