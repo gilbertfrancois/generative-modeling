8bbcaa97eb10535c3097981a79a310aedb93fe15
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2020-02-24 22:36:04 +0100

    Updates plotting of samples

--- tests/test_gaussian_sampler.py
+++ tests/test_gaussian_sampler.py
@@ -15,12 +15,24 @@ def main():
     clf = BayesClassifierGaussian()
     clf.fit(X, Y)
 
-    fig, axs = plt.subplots(nrows=1, ncols=10, squeeze=True)
+    fig, axs = plt.subplots(2, 10, figsize=(10, 2))
     for k in range(10):
         # sample from a given cluster k.
-        sample, mean = clf.sample_given_y(k)
-        axs[0][k].imshow(sample.reshape(28, 28))
-        axs[1][k].imshow(mean.reshape(28, 28))
+        res = clf.sample_given_y(k)
+        axs[0, k].imshow(res["sample"].reshape(28, 28))
+        axs[1, k].imshow(res["mean"].reshape(28, 28))
+        axs[0, k].axis("off")
+        axs[1, k].axis("off")
+    fig.suptitle("Sampling $P(x|y)$ from a single Gaussian per class")
+    plt.show()
+
+    # Sample from a random class.
+    res = clf.sample()
+    plt.figure()
+    fig, axs = plt.subplots(2, 1, figsize=(1, 2))
+    axs[0].imshow(res["sample"].reshape(28, 28))
+    axs[1].imshow(res["mean"].reshape(28, 28))
+    axs[0].set_title(res["class"])
     plt.show()
 
 
